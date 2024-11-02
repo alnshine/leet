9e3f197d7523630062baf322a8b2f0c7fe88c4dc
Abdykalykov Alnur <abdykalykovalnur@gmail.com>
2024-11-02 20:12:16 +0500

    add 2239

---
+++ find-closest-number-to-zero/2239.go
@@ -0,0 +1,21 @@
+package find_closest_number_to_zero
+
+import "math"
+
+func findClosestNumber(nums []int) int {
+	var res int
+	minN := math.MaxInt32
+	for i := 0; i < len(nums); i++ {
+		val := nums[i]
+		if val < 0 {
+			val *= -1
+		}
+		if val < minN {
+			minN = val
+			res = nums[i]
+		} else if val == minN && res < nums[i] {
+			res = nums[i]
+		}
+	}
+	return res
+}
