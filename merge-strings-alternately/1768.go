ff061d5bb92a45e9f38eeb5c4884d8ff737a288d
Abdykalykov Alnur <abdykalykovalnur@gmail.com>
2024-11-04 10:04:42 +0500

    fix

--- merge-strings-alternately/1768.go
+++ merge-strings-alternately/1768.go
@@ -8,9 +8,9 @@ func mergeAlternately(word1 string, word2 string) string {
 		i++
 	}
 	if len(word1) < len(word2) {
-		res += word2[i:len(word2)]
+		res += word2[i:]
 	} else if len(word2) < len(word1) {
-		res += word1[i:len(word1)]
+		res += word1[i:]
 	}
 	return res
 }
